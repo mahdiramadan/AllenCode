1dda2324edb49b8854f44449f984fc18ef3ea195
Mahdi Ramadan <mramadan@uw.edu>
2016-07-13 14:41:13 -0700

    added image segmentation method

--- image_processing.py
+++ image_processing.py
@@ -10,18 +10,21 @@ from synced_videos import SyncedVideos as sv
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import scipy
+from scipy import signal
 
 class ImageProcessing:
     def __init__(self, exp_folder, lims_ID):
 
         for file in os.listdir(exp_folder):
             # looks for the excel file and makes the directory to it
-            if file.endswith(".mp4") and file.startswith(lims_ID):
+            if file.endswith(".mp4") and file.startswith('output'):
                 self.directory = exp_folder
                 self.file_string = os.path.join(exp_folder, file)
                 self.sb = sb(exp_folder)
                 self.ep = ep(exp_folder, lims_ID)
                 self.sv = sv (exp_folder, lims_ID)
+                self.video_pointer = cv2.VideoCapture(self.file_string)
 
         if os.path.isfile(self.file_string):
                 self.data_present = True
@@ -93,6 +96,95 @@ class ImageProcessing:
 
 
 
+    def image_segmentation(self):
 
+        # get frame
+        ret, frame = self.video_pointer.read()
+
+        # convert to grey scale
+        img_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        self.show_frame(img_grey)
+
+        # apply gaussian blur filter
+        gauss = cv2.GaussianBlur(img_grey, (7, 7), 0)
+        self.show_frame(gauss)
+
+        # increase contrast
+        img_contrast = self.image_contrast(gauss)
+        self.show_frame(img_contrast)
+
+        # threshold
+        ret, img = cv2.threshold(img_contrast,220, 255, cv2.THRESH_OTSU)
+        self.show_frame(img)
+
+        # sharpen image
+        img_sharp = self.sharpen_image(img)
+        self.show_frame(img_sharp)
+
+        # noise removal
+        kernel = np.ones((3, 3), np.uint8)
+        opening = cv2.morphologyEx(img_sharp, cv2.MORPH_OPEN,kernel, iterations=3)
+        self.show_frame(opening)
+
+
+        # background
+        sure_bg = cv2.dilate(opening, kernel, iterations=3)
+        self.show_frame(sure_bg)
+
+        # foreground
+        dist_transform = cv2.distanceTransform(opening, cv2.cv.CV_DIST_L2, 5)
+        ret, sure_fg = cv2.threshold(dist_transform, 0.05 * dist_transform.max(), 255, 0)
+        self.show_frame(sure_fg)
+
+        # unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg, sure_fg)
+        self.show_frame(unknown)
+
+
+
+
+
+    def show_frame(self, frame):
+        cv2.imshow('image', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def image_contrast(self, img_grey):
+
+        # increase contrast
+        # alpha controls gain (contrast)
+        self.array_alpha = np.array([0.5])
+        # beta controls bias (brightness)
+        self.array_beta = np.array([-100.0])
+
+        # add a beta value to every pixel
+        cv2.add(img_grey, self.array_beta, img_grey)
+
+        # multiply every pixel value by alpha
+        cv2.multiply(img_grey, self.array_alpha, img_grey)
+
+        return img_grey
+
+
+    def sharpen_image(self, image):
+
+        kernel = np.zeros((9, 9), np.float32)
+        # Identity, times two!
+        kernel[4, 4] = 8.0
+
+        # Create a box filter:
+        boxFilter = np.ones((9, 9), np.float32) / 81.0
+
+        # Subtract the two:
+        kernel = kernel - boxFilter
+
+        # Note that we are subject to overflow and underflow here...but I believe that
+        # filter2D clips top and bottom ranges on the output, plus you'd need a
+        # very bright or very dark pixel surrounded by the opposite type.
+
+        image = cv2.filter2D(image, -1, kernel)
+
+        return image
 
 
