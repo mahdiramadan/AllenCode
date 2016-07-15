c5fc27c0754aba4dc176eb3c9afb149a43647592
Mahdi Ramadan <mramadan@uw.edu>
2016-07-14 22:47:42 -0700

    added new glcm method, gradient descent method

--- image_processing.py
+++ image_processing.py
@@ -3,15 +3,22 @@ This program will be used for video/image processing of
 behavior video
 """
 import os
-import cv2
 from stimulus_behavior import StimulusBehavior as sb
 from excel_processing import ExcelProcessing as ep
 from synced_videos import SyncedVideos as sv
 import numpy as np
-from tqdm import tqdm
 import matplotlib.pyplot as plt
+import cv2
+import skimage
+from skimage.feature import greycomatrix, greycoprops
+import tqdm
+import time
+from skimage import data
 import scipy
-from scipy import signal
+from scipy.optimize import minimize
+from scipy.spatial import distance
+import math
+
 
 class ImageProcessing:
     def __init__(self, exp_folder, lims_ID):
@@ -95,68 +102,20 @@ class ImageProcessing:
         return normal_wheel
 
 
-
-    def image_segmentation(self):
-
-        # get frame
-        ret, frame = self.video_pointer.read()
-
-        # convert to grey scale
-        img_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        self.show_frame(img_grey)
-
-        # apply gaussian blur filter
-        gauss = cv2.GaussianBlur(img_grey, (7, 7), 0)
-        self.show_frame(gauss)
-
-        # increase contrast
-        img_contrast = self.image_contrast(gauss)
-        self.show_frame(img_contrast)
-
-        # threshold
-        ret, img = cv2.threshold(img_contrast,220, 255, cv2.THRESH_OTSU)
-        self.show_frame(img)
-
-        # sharpen image
-        img_sharp = self.sharpen_image(img)
-        self.show_frame(img_sharp)
-
-        # noise removal
-        kernel = np.ones((3, 3), np.uint8)
-        opening = cv2.morphologyEx(img_sharp, cv2.MORPH_OPEN,kernel, iterations=3)
-        self.show_frame(opening)
-
-
-        # background
-        sure_bg = cv2.dilate(opening, kernel, iterations=3)
-        self.show_frame(sure_bg)
-
-        # foreground
-        dist_transform = cv2.distanceTransform(opening, cv2.cv.CV_DIST_L2, 5)
-        ret, sure_fg = cv2.threshold(dist_transform, 0.05 * dist_transform.max(), 255, 0)
-        self.show_frame(sure_fg)
-
-        # unknown region
-        sure_fg = np.uint8(sure_fg)
-        unknown = cv2.subtract(sure_bg, sure_fg)
-        self.show_frame(unknown)
-
-
-
-
-
     def show_frame(self, frame):
         cv2.imshow('image', frame)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def image_contrast(self, img_grey):
+    def image_contrast(self, img_grey, alpha, beta):
 
         # increase contrast
+        alpha = float(alpha)
+        beta = float(beta)
         # alpha controls gain (contrast)
-        self.array_alpha = np.array([0.5])
+        self.array_alpha = np.array([alpha])
         # beta controls bias (brightness)
-        self.array_beta = np.array([-100.0])
+        self.array_beta = np.array([beta])
 
         # add a beta value to every pixel
         cv2.add(img_grey, self.array_beta, img_grey)
@@ -167,11 +126,11 @@ class ImageProcessing:
         return img_grey
 
 
-    def sharpen_image(self, image):
+    def sharpen_image(self, image, value):
 
         kernel = np.zeros((9, 9), np.float32)
         # Identity, times two!
-        kernel[4, 4] = 8.0
+        kernel[4, 4] = value
 
         # Create a box filter:
         boxFilter = np.ones((9, 9), np.float32) / 81.0
@@ -186,5 +145,198 @@ class ImageProcessing:
         image = cv2.filter2D(image, -1, kernel)
 
         return image
+
+    def select_foreground(self,frame):
+
+        # convert to grey scale
+        img_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # apply gaussian blur filter
+        gauss = cv2.GaussianBlur(img_grey, (7, 7), 0)
+
+        # increase contrast
+        img_contrast = self.image_contrast(gauss, 0.5, -100)
+
+        # threshold
+        ret, img = cv2.threshold(img_contrast, 220, 255, cv2.THRESH_OTSU)
+
+        # sharpen image
+        img_sharp = self.sharpen_image(img, 8.0)
+
+        # noise removal
+        kernel = np.ones((3, 3), np.uint8)
+        opening = cv2.morphologyEx(img_sharp, cv2.MORPH_OPEN, kernel, iterations=2)
+
+        # background
+        sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+        # foreground
+        dist_transform = cv2.distanceTransform(opening, cv2.cv.CV_DIST_L2, 5)
+        ret, sure_fg = cv2.threshold(dist_transform, 0.05 * dist_transform.max(), 255, 0)
+
+        # unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv2.subtract(sure_bg, sure_fg)
+
+        # image processed without background (size is 480 x 640)
+        for i in range(0, 480):
+            if i in range(0, 140) or i in range(400, 480):
+                img_grey[i, :] = 255
+            else:
+                for j in range(0, 640):
+                    if sure_fg[i, j] != 0:
+                        img_grey[i, j] = 255
+
+        return img_grey
+
+
+    def image_segmentation(self):
+
+        # get frame
+        ret, frame = self.video_pointer.read()
+
+        # select foreground
+        foreground = self.select_foreground(frame)
+        # select mouse
+        mouse = self.select_mouse(foreground)
+
+
+    def select_mouse(self):
+        ret, frame = self.video_pointer.read()
+
+        self.gradient_descent(frame)
+
+        # dist = 0
+        # alpha = 0
+        # beta = 0
+        # zet = 0
+        # for m in range(-10,10):
+        #     for k in range (-100, 100):
+        #         for l in range (0, 20):
+        #             v = self.generate_glcm(m,k,l, frame)
+        #             if v > dist:
+        #                 dist = v
+        #                 alpha = m
+        #                 beta = k
+        #                 zet = l
+        # print(dist, alpha, beta, zet)
+
+
+    def generate_glcm(self, x, y, z, img):
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        PATCH_SIZE = 150
+
+        # increase contrast
+        alpha = float(x)
+        beta = float(y)
+        # alpha controls gain (contrast)
+        self.array_alpha = np.array([alpha])
+        # beta controls bias (brightness)
+        self.array_beta = np.array([beta])
+
+        # add a beta value to every pixel
+        cv2.add(img, self.array_beta, img)
+
+        # multiply every pixel value by alpha
+        cv2.multiply(img, self.array_alpha, img)
+
+        kernel = np.zeros((9, 9), np.float32)
+        # Identity, times two!
+        kernel[4, 4] = z
+
+        # Create a box filter:
+        boxFilter = np.ones((9, 9), np.float32) / 81.0
+
+        # Subtract the two:
+        kernel = kernel - boxFilter
+
+        # Note that we are subject to overflow and underflow here...but I believe that
+        # filter2D clips top and bottom ranges on the output, plus you'd need a
+        # very bright or very dark pixel surrounded by the opposite type.
+
+        image = cv2.filter2D(img, -1, kernel)
+
+        img = image
+
+        head_locations = [(150, 200), (150, 250), (150, 400), (150, 600), (150, 560), (150, 500), (160, 460),
+                          (170, 420)]
+        head_patches = []
+        for loc in head_locations:
+            head_patches.append(img[loc[0]:(loc[0] + PATCH_SIZE),
+                                loc[1]:(loc[1] + PATCH_SIZE)])
+            # To visualize areas, uncomment line below
+            # cv2.circle(image, (loc[1], loc[0]), 10, 255)
+
+        mouse_locations = [(200, 200), (200, 300), (200, 400), (300, 400), (300, 500), (320, 550), (320, 480),
+                           (230, 200)]
+        mouse_patches = []
+        for loc in mouse_locations:
+            mouse_patches.append(img[loc[0]:(loc[0] + PATCH_SIZE),
+                                 loc[1]:(loc[1] + PATCH_SIZE)])
+            # cv2.circle(image2, (loc[1], loc[0]), 10, 255)
+
+        xs = []
+        ys = []
+
+        for patch in (head_patches + mouse_patches):
+            glcm = greycomatrix(patch, [5], [0], 256, symmetric=True, normed=True)
+            xs.append(greycoprops(glcm, 'dissimilarity')[0, 0])
+            ys.append(greycoprops(glcm, 'correlation')[0, 0])
+
+        head = []
+        mouse = []
+
+        for i in range(len(head_patches)):
+            head.append((xs[i], ys[i]))
+
+        for i in range(len(head_patches), len(xs)):
+            mouse.append((xs[i], ys[i]))
+
+        dist = []
+        for i in range(len(head)):
+                k = head[i]
+                l = mouse[i]
+                dist.append(math.sqrt(math.pow((k[0]-l[0]), 2) + math.pow((k[1] - l[1]), 2)))
+        total_dist = sum(dist)
+
+        return total_dist
+
+
+    def gradient_descent(self, frame):
+
+        x = -10
+        y = -100
+        z = 0
+        limit = 1000
+
+        dist_old = self.generate_glcm(x,y,z,frame)
+        dist_new = 100
+        step = 10
+        count = 0
+
+        while (count) < limit:
+            dist_new = self.generate_glcm(x + step, y + step, z + step, frame)
+            if dist_new >= dist_old:
+                x = x + step
+                y = y + step
+                z = z + step
+                dist_old = dist_new
+
+            else:
+                step = step-1
+
+            count += 1
+        print (x,y,z, dist_new)
+
+
+
+
+
+
+
+
+
 
 
