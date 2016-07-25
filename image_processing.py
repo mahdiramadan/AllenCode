9563090596e5fb80868a3c60d6b1bb9cfccf62d2
Mahdi Ramadan <mramadan@uw.edu>
2016-07-24 20:42:31 -0700

    added SVC parameter search

--- image_processing.py
+++ image_processing.py
@@ -10,11 +10,11 @@ from wheel_data import WheelData as wd
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import sys
-import pickle
+import pandas
 import time
-from skimage.feature import hog
-from skimage import data, color, exposure
+import tables
+import h5py
+
 
 
 class ImageProcessing:
@@ -39,177 +39,69 @@ class ImageProcessing:
     def is_valid(self):
         return self.data_present
 
-    def run_whole_video(self):
-
-        wheel_data = self.wd.normalize_wheel_data()
-        # self.video_pointer.set(1, 36200)
-        ret, frame = self.video_pointer.read()
-        frames = []
-        opticals = []
-        angles = []
-        prvs = cv2.cvtColor(frame[160:420, 100:640], cv2.COLOR_BGR2GRAY)
-        ret, frame = self.video_pointer.read()
-        # prvs = self.image_segmentation(frame)
-        frames.append(prvs)
-        # hsv = np.zeros_like(frame[160:420, 100:640])
-        # hsv[..., 1] = 255
-        count = 0
-
-        while count < len(wheel_data):
-
-            frame = cv2.cvtColor(frame[160:420, 100:640], cv2.COLOR_BGR2GRAY)
-            next = frame
-            frames.append(next)
-            # data = self.image_segmentation(frame)
-            # frames.append(data['image'])
-            optical = self.optical_flow(prvs, next)
-            opticals.append(optical['mag'])
-            angles.append(optical['ang'])
-            # hsv[..., 0] = optical['ang'] * 180 / np.pi / 2
-            # hsv[..., 2] = cv2.normalize(optical['mag'], None, 0, 255, cv2.NORM_MINMAX)
-            # rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-            #
-            # cv2.imshow('frame2', rgb)
-            # k = cv2.waitKey(30) & 0xff
-            # if k == 27:
-            #     break
-            # elif k == ord('s'):
-            #     cv2.imwrite('opticalfb.png', next)
-            #     cv2.imwrite('opticalhsv.png', rgb)
-
-            prvs = next
-            ret, frame = self.video_pointer.read()
-            count += 1
-
-        with open("optical.pickle", 'wb') as f:
-            pickle.dump(opticals, f)
-
-        with open("angle.pickle", 'wb') as f:
-            pickle.dump(angles, f)
-
-        with open("wheel.pickle", 'wb') as f:
-            pickle.dump(wheel_data, f)
-
-        with open("frames.pickle", 'wb') as f:
-            pickle.dump(frames, f)
-
-
-        # self.video_pointer.release()
-        # cv2.destroyAllWindows()
-        #     # prvs = data['image']
-
-
-
-    def image_segmentation(self, frame):
-
-        # tail = self.detect_tail(frame)
-
-        # calculate frame dimensions
-        height = len(frame)
-        width = len(frame[1])
-
-        # # select foreground
-        # foreground = self.select_foreground(frame, width, height)
-
-        # crop image
-        where = 160
-        crop = self.crop_image(frame, where)
-
-        return {'image': crop}
-
     def show_frame(self, frame):
         cv2.imshow('image', frame)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def image_contrast(self, img_grey, alpha, beta):
+    def run_whole_video(self):
 
-        # increase contrast
-        alpha = float(alpha)
-        beta = float(beta)
-        # alpha controls gain (contrast)
-        self.array_alpha = np.array([alpha])
-        # beta controls bias (brightness)
-        self.array_beta = np.array([beta])
+        # wheel_data = self.wd.normalize_wheel_data()
 
-        # add a beta value to every pixel
-        cv2.add(img_grey, self.array_beta, img_grey)
+        hf = h5py.File('data.h5', 'w')
 
-        # multiply every pixel value by alpha
-        cv2.multiply(img_grey, self.array_alpha, img_grey)
+        # self.video_pointer.set(1, 30000)
+        ret, frame = self.video_pointer.read()
 
-        return img_grey
+        frame = cv2.cvtColor(frame[160:420, 100:640], cv2.COLOR_BGR2GRAY)
 
-    def sharpen_image(self, image, value):
+        prvs = frame
+        next = frame
 
-        kernel = np.zeros((9, 9), np.float32)
-        # Identity, times two!
-        kernel[4, 4] = value
+        count = 0
+        mod = 0
+        opticals = []
+        angles = []
+        frames = []
 
-        # Create a box filter:
-        boxFilter = np.ones((9, 9), np.float32) / 81.0
+        limit = self.wd.wheel_data_length()
 
-        # Subtract the two:
-        kernel = kernel - boxFilter
-
-        # Note that we are subject to overflow and underflow here...but I believe that
-        # filter2D clips top and bottom ranges on the output, plus you'd need a
-        # very bright or very dark pixel surrounded by the opposite type.
-
-        image = cv2.filter2D(image, -1, kernel)
-
-        return image
-
-    def select_foreground(self, frame, width, height):
-
-        # convert to grey scale
-        img_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # apply gaussian blur filter
-        gauss = cv2.GaussianBlur(img_grey, (7, 7), 0)
-
-        # increase contrast
-        img_contrast = self.image_contrast(gauss, 0.5, -100)
-
-        # threshold
-        ret, img = cv2.threshold(img_contrast, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-        # sharpen image
-        img_sharp = self.sharpen_image(img, 8.0)
-
-        # noise removal
-        kernel = np.ones((3, 3), np.uint8)
-        opening = cv2.morphologyEx(img_sharp, cv2.MORPH_OPEN, kernel, iterations=2)
-
-        # select background
-        sure_bg = cv2.dilate(opening, kernel, iterations=3)
-
-        # select foreground
-        dist_transform = cv2.distanceTransform(opening, cv2.cv.CV_DIST_L2, 5)
-        ret, sure_fg = cv2.threshold(dist_transform, 0.05 * dist_transform.max(), 255, 0)
-
-        # unknown region
-        sure_fg = np.uint8(sure_fg)
-        unknown = cv2.subtract(sure_bg, sure_fg)
+        while count <= limit:
+            if count > 0:
+                if count%1000 == 0 or count == limit:
+                    mod += 1
+                    g = hf.create_group('first ' + str(mod) + '000 frames')
+                    g.create_dataset('frames', data=frames, compression="gzip", compression_opts=9)
+                    g.create_dataset('optical', data=opticals, compression="gzip", compression_opts=9)
+                    g.create_dataset('angles', data=angles, compression="gzip", compression_opts=9)
 
 
-        return {'sure_fg': sure_fg, 'unknown': unknown}
+                    frames = []
+                    opticals = []
+                    angles = []
 
-    def crop_image(self, frame, where):
+            prvs = next
+            frames.append(prvs)
 
-        # image processed without background (size is 480 x 640), based
-        # on output of threshold function in select_foreground
+            ret, frame = self.video_pointer.read()
+            next = cv2.cvtColor(frame[160:420, 100:640], cv2.COLOR_BGR2GRAY)
 
-        frame = frame[where:420, 100:640]
+            optical = self.optical_flow(prvs, next)
+            opticals.append(optical['mag'])
+            angles.append(optical['ang'])
 
-        return frame
+            count += 1
+
+            if count%1000 == 0:
+                print (count)
 
     def optical_flow(self, prvs, next):
 
-        # prvs = np.asarray(prvs)
-        # next = np.asarray(next)
 
         flow = cv2.calcOpticalFlowFarneback(prvs, next, 0.5, 3, 15, 3, 5, 1.2, 0)
-        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        mag, ang= cv2.cartToPolar(flow[..., 0], flow[..., 1])
+
+        mag = np.int16(mag)
+        ang = np.int16(ang*180/np.pi/2)
 
         return {'mag': mag, 'ang': ang}
