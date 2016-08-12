548f2ab23f2ab4ffee6f3440cc07d8c5a9776490
Mahdi Ramadan <mramadan@uw.edu>
2016-08-12 14:06:20 -0700

    new preprocessing creates frame length x feature space matrix

--- image_processing.py
+++ image_processing.py
@@ -17,6 +17,9 @@ import h5py
 from sklearn import preprocessing
 from multiprocessing import Pool
 import warnings
+from math import isnan
+from sklearn.externals import joblib
+from sklearn.preprocessing import Imputer
 
 
 
@@ -26,65 +29,74 @@ def show_frame(frame):
     cv2.destroyAllWindows()
 
 def run_whole_video(exp_folder, lims_ID):
-
+    #initializes video pointer for video of interest based on lims ID
     file_string = get_file_string(exp_folder, lims_ID)
     video_pointer = cv2.VideoCapture(file_string)
 
-    hf = h5py.File('data_'+str(lims_ID)+'.h5', 'w')
+    # import wheel data
+    wheel = joblib.load('dxds2.pkl')
+    first_non_nan = next(x for x in wheel if not isnan(x))
+    first_index = np.where(wheel == first_non_nan)[0]
+    k = first_index[0]
+    imp = Imputer(missing_values='NaN', strategy='mean')
+    wheel = imp.fit_transform(wheel)
+    wheel = preprocessing.MinMaxScaler((-1, 1)).fit(wheel).transform(wheel)
 
     # self.video_pointer.set(1, 41000)
     ret, frame = video_pointer.read()
 
-
+    # crops and converts frame into desired format
     frame = cv2.cvtColor(frame[160:400, 100:640], cv2.COLOR_BGR2GRAY)
 
     prvs = frame
-    next = frame
+    nex = frame
 
+    # initialize vectors to keep track of data
     count = 0
     mod = 0
     opticals = []
     angles = []
     frames = []
 
-
+    # length of movie
     limit = int(video_pointer.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
 
-    while count <= limit:
-        if count > 0:
-            if count%1000 == 0 or count == limit:
-                mod += 1
-                g = hf.create_group('first ' + str(mod) + '000 frames')
-                g.create_dataset('frames', data=frames, compression="gzip", compression_opts=9)
-                g.create_dataset('optical', data=opticals, compression="gzip", compression_opts=9)
-                g.create_dataset('angles', data=angles, compression="gzip", compression_opts=9)
+
+    # create hdf file
+    hf = h5py.File('data_' + str(lims_ID) + '.h5', 'w')
+    g = hf.create_group('feature space')
+    vector = np.zeros((100000, 4321))
+    table = g.create_dataset('features', data=vector, compression='gzip', compression_opts=9, shape=(100000, 4321))
 
 
-                frames = []
-                opticals = []
-                angles = []
+    while count <= 10000:
 
-        prvs = next
-        frames.append(process_input(prvs))
+        prvs = nex
+        frames = process_input(prvs)
 
         ret, frame = video_pointer.read()
-        next = cv2.cvtColor(frame[160:400, 100:640], cv2.COLOR_BGR2GRAY)
+        nex = cv2.cvtColor(frame[160:400, 100:640], cv2.COLOR_BGR2GRAY)
 
-        optical = optical_flow(prvs, next)
-        opticals.append(optical['mag'])
-        angles.append(optical['ang'])
+        optical = optical_flow(prvs, nex)
+        opticals = optical['mag']
+        angles= optical['ang']
+        vector_data = np.concatenate((np.reshape(wheel[k], (1)), frames, opticals, angles))
+
+        table[count, :] = np.float16(vector_data)
 
         count += 1
+
 
         if count%1000 == 0:
             print (count)
 
 def optical_flow(prvs, next):
 
-
+    # calculate optical flow and angle of out two frame
     flow = cv2.calcOpticalFlowFarneback(prvs, next, 0.5, 3, 15, 3, 5, 1.2, 0)
     mag, ang= cv2.cartToPolar(flow[..., 0], flow[..., 1])
 
+    # get histograms of optical flow and angles (these are our features)
     mag = process_input(mag)
     ang = process_input((ang*180/np.pi/2))
 
@@ -93,7 +105,8 @@ def optical_flow(prvs, next):
 def process_input(input):
 
     frame_data = []
-
+    # for each defined window over the data, bin values and return a
+    # properly scaled list of expectation values
     for (x, y, window) in sliding_window(input, 30, (30, 30)):
         hist, bin = np.histogram(window, 10)
         center = (bin[:-1] + bin[1:]) / 2
@@ -119,10 +132,10 @@ def get_file_string(exp_folder,lims_ID):
 
 
 if __name__ == '__main__':
-
+    # initializes code with path to folder and lims ID
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    exp_folder = '/Users/mahdiramadan/Documents/Allen_Institute/code_repository/Data/501560436'
-    lims_ID = '501560436'
+    exp_folder = '/Users/mahdiramadan/Documents/Allen_Institute/code_repository/Data/497060401'
+    lims_ID = '497060401'
     run_whole_video(exp_folder, lims_ID)
 
     # p = Process(target=run_whole_video(video_pointer, lims_ID), args= (video_pointer, lims_ID))
